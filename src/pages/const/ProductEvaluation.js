3071c42802bf4716f453db739474b4c166ec969c
Osvaldoguzman17 <julioosvaldoguzmancorrea53@gmail.com>
2025-05-30 13:15:09 -0600

    Fix: remove unused FaStar import

--- src/pages/const/ProductEvaluation.js
+++ src/pages/const/ProductEvaluation.js
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { FaStar, FaArrowLeft, FaSpinner } from "react-icons/fa";
+import { FaArrowLeft, FaSpinner } from "react-icons/fa";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
